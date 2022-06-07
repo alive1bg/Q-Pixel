8b2ab53bfdebff87be13481f5955d43345f0f19e
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-07 05:49:05 +0100

    another oneeee

---
+++ resources/[assets_vehicles]/temp_vh/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files {
+    'metas/carcols.meta',
+    'metas/carvariations.meta',
+    'metas/vehicles.meta',
+    'metas/handling.meta',
+    'metas/vehiclelayouts.meta',
+	'metas/shop_vehicle',
+}
+
+data_file 'HANDLING_FILE' 'metas/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/vehicles.meta'
+data_file 'CARCOLS_FILE' 'metas/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'metas/vehiclelayouts.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'metas/shop_vehicle.meta'
