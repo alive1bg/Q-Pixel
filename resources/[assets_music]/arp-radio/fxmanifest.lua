832fa9b5befcec5aa06040f890c091e00f26aac6
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-07 01:58:47 +0100

    13k left.

---
+++ resources/[assets_music]/arp-radio/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'PopoAU'
+description 'This resource allows you to stream custom radio files'
+version '0.0.1'
+
+files {
+	'RADIO_14_DANCE_02/flylo_part1.awc',
+	'RADIO_14_DANCE_02/flylo_part2.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'RADIO_14_DANCE_02'
