aadeb54254d1ea65b8381a640478b4ff789a87ff
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-15 02:03:00 +0100

    [qb-vote] - Fixed Error With Item

--- resources/[resources]/[qb]/qb-vote/server.lua
+++ resources/[resources]/[qb]/qb-vote/server.lua
@@ -13,6 +13,7 @@ end)
 
 RegisterServerEvent("qb-vote:server:postedVote")
 AddEventHandler("qb-vote:server:postedVote", function(who)
+    local src = source
     local xPlayer = QBCore.Functions.GetPlayer(source)
     vote[GetPlayerIdentifiers(source)[1]] = "voted"
     if vote[who] == nil then
@@ -21,13 +22,11 @@ AddEventHandler("qb-vote:server:postedVote", function(who)
         vote[who] = vote[who] + 1
     end
     SaveResourceFile(GetCurrentResourceName(), "data.json", json.encode(vote))
-    TriggerClientEvent("QBCore:Notify", src, "Your vote has been accepted", 'success', 7500)
-
     TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items["np_voted_sticker"], 'add')
-    xPlayer.Functions.AddItem('np_voted_sticker',  1, false, who) -- set to 1 still pulls the config spawned ammount...
+    xPlayer.Functions.AddItem('np_voted_sticker',  1, false) -- set to 1 still pulls the config spawned ammount...
 end)
  
-CreateThread(function()
+CreateThread(function() 
     local result = json.decode(LoadResourceFile(GetCurrentResourceName(), "data.json"))
 
     if result then
