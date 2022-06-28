ce465f38c3139e60648c61a9f31eec884fcb6c31
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-28 14:43:07 +0100

    np-misc in and working.

---
+++ resources/[assets_maps]/[npmlos]/np-misc/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+	'popcycle.dat'
+}
+
+data_file 'POPSCHED_FILE' 'popcycle.dat'
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/np_misc_load.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/interior_props/discoverybuilder.ytyp'
