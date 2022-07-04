83f28d35393722582e71dc2ead3e4d3fe6eaf59c
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-07-04 19:03:08 +0100

    Revert "Dev stuff ;) ;) ;) KR here"
    This reverts commit f53316b33f9c670f4cd3fd6376374bcb21e5b419.

--- resources/[dev]/apix-crafting/s_main.lua
+++
@@ -1,7 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('apix-crafting:Server:AddXP', function(xp, amount)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.SetMetaData("craftingrep", Player.PlayerData.metadata["craftingrep"]+(xp*amount))
-end)
