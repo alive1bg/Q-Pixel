54e61c070daff0fc5d1f228d81771204ac1f01c4
Rhodinium <karxidev@gmail.com>
2022-07-06 19:21:31 +0100

    New Crosshair that isnt straight meta. #NP

--- resources/[resources]/[qb]/qb-smallresources/client/hudcomponents.lua
+++ resources/[resources]/[qb]/qb-smallresources/client/hudcomponents.lua
@@ -13,7 +13,7 @@ CreateThread(function()-- https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
 		HideHudComponentThisFrame(11)		-- 11 : FLOATING_HELP_TEXT_1
 		HideHudComponentThisFrame(12)		-- 12 : FLOATING_HELP_TEXT_2
 		HideHudComponentThisFrame(13) -- 13 : CASH_CHANGE
-		--HideHudComponentThisFrame(14) -- 14 : RETICLE
+		HideHudComponentThisFrame(14) -- 14 : RETICLE
 		HideHudComponentThisFrame(15)		-- 15 : SUBTITLE_TEXT
 		-- HideHudComponentThisFrame(16)		-- 16 : RADIO_STATIONS
 		HideHudComponentThisFrame(17) -- 17 : SAVING_GAME
