a27d8977cab5494c929d3c52629654417352e9ab
Rhodinium <karxidev@gmail.com>
2022-07-10 02:44:22 +0100

    Update cl_main.lua

--- resources/[resources]/[new]/zd-pursuitmods/client/cl_main.lua
+++ resources/[resources]/[new]/zd-pursuitmods/client/cl_main.lua
@@ -45,40 +45,40 @@ RegisterCommand("pursuit", function(source, args)
             break
         end
     end
-    if  PlayerData.job.name == 'police' then 
-    if not vehiclePresetName then return end
-    if currentLevel == 3 then currentLevel = 1 end
-    currentLevel = currentLevel + 1
-    local modLevel = vehiclePresetMods[currentLevel]
-    SetVehicleXenonLightsColour(vehicle, modLevel.appearance.colors.xenon)
-    ToggleVehicleMod(vehicle, modLevel.mods.Turbo)
-    SetVehicleMod(vehicle, 11, modLevel.mods.Engine, false)
-    SetVehicleMod(vehicle, 12, modLevel.mods.Brakes, false)
-    SetVehicleMod(vehicle, 13, modLevel.mods.Transmission, false)
-    SetVehicleMod(vehicle, 22, modLevel.mods.XenonHeadlights, false)
-    for i = 1, #modLevel.handling do
-        if not DecorIsRegisteredAsType(modLevel.handling[i].field, 3) then 
-            DecorRegister(modLevel.handling[i].field, 3)
+    --if  PlayerData.job.name == 'police' then 
+        if not vehiclePresetName then return end
+        if currentLevel == 3 then currentLevel = 1 end
+        currentLevel = currentLevel + 1
+        local modLevel = vehiclePresetMods[currentLevel]
+        SetVehicleXenonLightsColour(vehicle, modLevel.appearance.colors.xenon)
+        ToggleVehicleMod(vehicle, modLevel.mods.Turbo)
+        SetVehicleMod(vehicle, 11, modLevel.mods.Engine, false)
+        SetVehicleMod(vehicle, 12, modLevel.mods.Brakes, false)
+        SetVehicleMod(vehicle, 13, modLevel.mods.Transmission, false)
+        SetVehicleMod(vehicle, 22, modLevel.mods.XenonHeadlights, false)
+        for i = 1, #modLevel.handling do
+            if not DecorIsRegisteredAsType(modLevel.handling[i].field, 3) then 
+                DecorRegister(modLevel.handling[i].field, 3)
+            end
+            Citizen.Wait(100)
+            local defaultValue = DecorGetFloat(vehicle, modLevel.handling[i].field)
+            if defaultValue == 0 then
+                defaultValue = GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field)
+                DecorSetFloat(vehicle, modLevel.handling[i].field, defaultValue)
+            end
         end
-        Citizen.Wait(100)
-        local defaultValue = DecorGetFloat(vehicle, modLevel.handling[i].field)
-        if defaultValue == 0 then
-            defaultValue = GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field)
-            DecorSetFloat(vehicle, modLevel.handling[i].field, defaultValue)
+        for i = 1, #modLevel.handling do 
+            print('Before',GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field))
+            SetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field, DecorGetFloat(vehicle, modLevel.handling[i].field)*modLevel.handling[i].multiplier)
+            print('After',GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field))
         end
-    end
-    for i = 1, #modLevel.handling do 
-        print('Before',GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field))
-        SetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field, DecorGetFloat(vehicle, modLevel.handling[i].field)*modLevel.handling[i].multiplier)
-        print('After',GetVehicleHandlingFloat(vehicle, 'CHandlingData', modLevel.handling[i].field))
-    end
-    if source then 
-        QBCore.Functions.Notify("Pursuit Mod: " ..modLevel.name)
-        print(modLevel.name)
-    end
-else
-    QBCore.Functions.Notify("U are not a Police.", "error")
-end
+        if source then 
+            QBCore.Functions.Notify("Pursuit Mod: " ..modLevel.name)
+            print(modLevel.name)
+        end
+    --else
+    --    QBCore.Functions.Notify("U are not a Police.", "error")
+    --end
 
 end)
 
