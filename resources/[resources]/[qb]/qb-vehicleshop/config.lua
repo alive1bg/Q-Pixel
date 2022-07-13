51bc016b3a9d42f6ad8668ceb4e5d6e22de92445
Rhodinium <karxidev@gmail.com>
2022-07-13 22:20:45 +0100

    Fixed ottos. POS xD

--- resources/[resources]/[qb]/qb-vehicleshop/config.lua
+++ resources/[resources]/[qb]/qb-vehicleshop/config.lua
@@ -22,7 +22,7 @@ Config.Shops = {
             ['size'] = 2.75 -- size of the vehicles zones
         },
         ['Job'] = 'cardealer', -- Name of job or none
-        ['ShopLabel'] = 'Otto AutoParts And Sales', -- Blip name
+        ['ShopLabel'] = 'Ottos', -- Blip name
         ['showBlip'] = true, -- true or false
         ['blipSprite'] = 326, -- Blip sprite
         ['blipColor'] = 3, -- Blip color
@@ -36,7 +36,7 @@ Config.Shops = {
             ['compacts'] = 'Compacts',
             ['motorcycles'] = 'Motorcycles',
             ['vans'] = 'Vans',
-            ['cycles'] = 'Bicycles'
+            ['cycles'] = 'Bicycles',
         },
         ['TestDriveTimeLimit'] = 0.5, -- Time in minutes until the vehicle gets deleted
         ['Location'] = vector3(801.00994, -822.8892, 26.181016), -- Blip Location
@@ -45,25 +45,25 @@ Config.Shops = {
         ['TestDriveSpawn'] = vector4(802.38647, -820.7225, 26.180999, 266.13333), -- Spawn location for test drive
         ['ShowroomVehicles'] = {
             [1] = {
-                coords = vector4(797.95074, -806.8751, 25.2, 234.14276), -- where the vehicle will spawn on display
+                coords = vector4(797.95074, -806.8751, 25.2000, 234.14276), -- where the vehicle will spawn on display
                 defaultVehicle = 'baller', -- Default display vehicle
                 chosenVehicle = 'baller', -- Same as default but is dynamically changed when swapping vehicles
             },
             [2] = {
-                coords = vector4(798.09246, -810.5512, 25.2, 237.48246),
+                coords = vector4(798.09246, -810.5512, 25.2000, 237.48246),
                 defaultVehicle = 'schafter2',
-                chosenVehicle = 'schafter2'
+                chosenVehicle = 'schafter2',
             },
             [3] = {
-                coords = vector4(798.27111, -813.7525, 25.2, 238.25881),
+                coords = vector4(798.27111, -813.7525, 25.2000, 238.25881),
                 defaultVehicle = 'comet2',
-                chosenVehicle = 'comet2'
+                chosenVehicle = 'comet2',
             },
             [4] = {
-                coords = vector4(797.99426, -816.7172, 25.2, 239.4253),
+                coords = vector4(797.99426, -816.7172, 25.2000, 239.4253),
                 defaultVehicle = 'sultanrs',
-                chosenVehicle = 'sultanrs'
-            }
+                chosenVehicle = 'sultanrs',
+            },
         },
     },
     ['tuner'] = {
@@ -156,7 +156,7 @@ Config.Shops = {
                 defaultVehicle = 'gtz34',
                 chosenVehicle = 'gtz34',
             },
-        }
+        },
     },
     ['boats'] = {
         ['Type'] = 'free-use', -- no player interaction is required to purchase a vehicle
