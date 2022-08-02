c1fec4a715993e15b094be20ff8ff2257565689e
zuentity <108105978+zuentity@users.noreply.github.com>
2022-08-02 07:53:54 -0700

    Fix 43/52

---
+++ resources/[assets_vehicles]/qb-vehicles/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+files {
+    'popgroups.ymt',
+    'popcycle.dat',
+    'vehiclelayouts.meta',
+    'handling.meta',
+    'vehicles.meta',
+    'carcols.meta',
+    'carvariations.meta',
+}
+
+data_file 'DLC_POP_GROUPS' 'popgroups.ymt'
+data_file 'POPSCHED_FILE' 'popcycle.dat'
+client_script 'vehicle_names.lua'
