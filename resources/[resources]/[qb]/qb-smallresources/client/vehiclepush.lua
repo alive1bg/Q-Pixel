d553a22edaa8992d4981e31ef7499965639ae064
BryBTG <50550050+BryBTG@users.noreply.github.com>
2022-08-03 14:27:15 +0800

    Fix vehicle push error

--- resources/[resources]/[qb]/qb-smallresources/client/vehiclepush.lua
+++ resources/[resources]/[qb]/qb-smallresources/client/vehiclepush.lua
@@ -1,3 +1,15 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
 local First = vector3(0.0, 0.0, 0.0)
 local Second = vector3(5.0, 5.0, 5.0)
 
@@ -36,16 +48,16 @@ end)
 Citizen.CreateThread(function()
     local lerpCurrentAngle = 0.0
 
-    while true do 
+    while true do
         Citizen.Wait(5)
         local ped = PlayerPedId()
         if Vehicle.Vehicle ~= nil then
             if IsControlPressed(0, Keys["LEFTSHIFT"]) and IsVehicleSeatFree(Vehicle.Vehicle, -1) and not IsEntityAttachedToEntity(ped, Vehicle.Vehicle) and IsControlJustPressed(0, Keys["E"]) then
                 if not IsEntityUpsidedown(Vehicle.Vehicle) then
-                    
+
                     NetworkRequestControlOfEntity(Vehicle.Vehicle)
                     local coords = GetEntityCoords(ped)
-                    if Vehicle.IsInFront then    
+                    if Vehicle.IsInFront then
                         AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y * -1 + 0.1 , Vehicle.Dimensions.z + 1.0, 0.0, 0.0, 180.0, 0.0, false, false, true, false, true)
                     else
                         AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y - 0.3, Vehicle.Dimensions.z  + 1.0, 0.0, 0.0, 0.0, 0.0, false, false, true, false, true)
@@ -66,9 +78,9 @@ Citizen.CreateThread(function()
                             lerpCurrentAngle = lerpCurrentAngle - speed
                         else
                             SetVehicleSteeringAngle(Vehicle.Vehicle, lerpCurrentAngle)
-    
+
                             --Slowly restore to center.
-                            if lerpCurrentAngle < -0.02 then    
+                            if lerpCurrentAngle < -0.02 then
                                 lerpCurrentAngle = lerpCurrentAngle + speed
                             elseif lerpCurrentAngle > 0.02 then
                                 lerpCurrentAngle = lerpCurrentAngle - speed
@@ -143,7 +155,7 @@ RegisterNetEvent("qb:flipvehicle", function()
 
                 QBCore.Functions.Notify("Vehicle flip failed!", "error")
 				FreezeEntityPosition(playerPed, false)
-				ClearPedTasks(playerPed)								
+				ClearPedTasks(playerPed)
 			end)
 		else
             QBCore.Functions.Notify("There is no vehicle nearby", "error")
